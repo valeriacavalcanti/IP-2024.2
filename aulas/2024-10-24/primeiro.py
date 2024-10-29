6b4a65a9c15968ac4e85a9a324af31ca1b25a17b
Valéria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2024-10-29 10:48:42 -0300

    Aula 24/10/2024

---
+++ aulas/2024-10-24/primeiro.py
@@ -0,0 +1,14 @@
+# ler: 3 notas
+nota1 = input('Digite 1º nota: ')
+nota2 = input('Digite 2º nota: ')
+nota3 = input('Digite 3º nota: ')
+nota1 = int(nota1)
+nota2 = int(nota2)
+nota3 = int(nota3)
+
+# fazer: somar as notas
+soma = nota1 + nota2 + nota3
+
+# exibir: a soma das notas
+print(nota1, nota2, nota3)
+print('soma', soma)
