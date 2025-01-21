c03d99f26d121ead79919aa076d2614d204aafca
Valeria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2025-01-21 16:31:56 -0300

    Aula 19/12/2024

---
+++ aulas/2024-12-19/etapa1.py
@@ -0,0 +1,8 @@
+nomes = [''] * 10
+
+# ler os nomes
+for i in range(10):
+    nomes[i] = input('Nome: ')
+
+# exibir os nomes
+print(nomes)
---
+++ aulas/2024-12-19/etapa6.py
@@ -0,0 +1,39 @@
+import random
+
+nomes = []
+
+# ler os nomes
+while True:
+    nome = input('Nome: ')
+    
+    if (nome == ''):
+        break
+
+    nomes.append(nome)
+
+# exibir os nomes
+print(nomes)
+
+# tamanho do vetor
+tamanho = len(nomes)
+
+for i in range(6):
+    # permutar duas posições aleatórias
+    i1 = random.randint(0, tamanho - 1)
+    i2 = random.randint(0, tamanho - 1)
+
+    aux = nomes[i1]
+    nomes[i1] = nomes[i2]
+    nomes[i2] = aux
+
+# exibir todos os nomes
+print(nomes)
+
+# sorter um nome
+i = random.randint(0, tamanho - 1)
+print(i, nomes[i])
+
+
+
+
+
